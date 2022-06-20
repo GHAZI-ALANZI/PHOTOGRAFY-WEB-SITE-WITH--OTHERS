e5baf86d8a2e52b2970f4134e946e2e9e0d3b843
Crackhoven <jan-fiedler@hotmail.com>
2022-06-20 15:12:49 +0200

    package added

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register',
@@ -11,5 +12,38 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+confirmBox() {
+  Swal.fire({
+    title: 'Are you sure want to remove?',
+    text: 'You will not be able to recover this file!',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'Yes, delete it!',
+    cancelButtonText: 'No, keep it',
+  }).then((result) => {
+    if (result.value) {
+      Swal.fire(
+        'Deleted!',
+        'Your imaginary file has been deleted.',
+        'success'
+      );
+    } else if (result.dismiss === Swal.DismissReason.cancel) {
+      Swal.fire('Cancelled', 'Your imaginary file is safe :)', 'error');
+    }
+  });
+}
+login(){
+  Swal.fire({
+    title:"Login",
+    html:
+    `<div class="form-floating mb-3">
+    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+    <label for="floatingInput">Email address</label>
+  </div>
+  ` + `<div class="form-floating">
+  <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+  <label for="floatingPassword">Password</label>
+</div>` + ``
+  })
+}
 }
