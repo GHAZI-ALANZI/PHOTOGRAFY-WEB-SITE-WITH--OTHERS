c6cf4237e7133b91345b5c6a6382aaeceb8d8ae4
Crackhoven <jan-fiedler@hotmail.com>
2022-06-20 13:59:32 +0200

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { GalleryAlbumComponent } from './gallery-album/gallery-album.component';
+import { GalleryPhotoComponent } from './gallery-photo/gallery-photo.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: "", component: HomeComponent
+},{
+  path:"contact", component: ContactComponent
+},{
+  path:"about", component: AboutComponent
+},{
+  path:"register", component: RegisterComponent
+},{
+  path:"login", component: LoginComponent
+},{
+  path:"gallery", component: GalleryAlbumComponent
+},{
+  path:"photos",component: GalleryPhotoComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
